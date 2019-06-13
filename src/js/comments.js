37fc9cab396fb90c726bd500066bd068725a8afa
Manuel Ignacio López Quintero <miloqu@gmail.com>
2019-06-13 11:29:49 +0200

    adding content

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,8 +1,7 @@
 document.write("<div id='disqus_thread'></div>");
-
-var disqus_shortname = 'milqweb';
-(function() {
-  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+(function() { 
+var d = document, s = d.createElement('script');
+s.src = 'https://milqweb.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
 })();
---
+++ src/js/comments_old.js
@@ -0,0 +1,8 @@
+document.write("<div id='disqus_thread'></div>");
+
+var disqus_shortname = 'milqweb';
+(function() {
+  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
