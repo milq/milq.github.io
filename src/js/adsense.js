188ac008e53891ad837b5c1a94f6308d912334b5
Manuel Ignacio López Quintero <miloqu@gmail.com>
2019-10-27 05:54:37 +0100

    Create ads.txt

---
+++ src/js/adsense.js
@@ -0,0 +1,7 @@
+document.write("<div id='disqus_thread'></div>");
+(function() { 
+var d = document, s = d.createElement('script');
+s.src = 'https://milqweb.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
