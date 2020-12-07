00eef23362ce57ab9ff39f0d7890a3f2fedd00e8
Manuel Ignacio López Quintero <miloqu@gmail.com>
2020-12-07 21:40:15 +0100

    Adding changes

---
+++ cursos/dwes/ud/3/inicio.sql
@@ -0,0 +1,25 @@
+-- Script de creación de bases de datos para MySQL/MariaDB
+
+DROP DATABASE IF EXISTS ud3_balance;
+DROP DATABASE IF EXISTS ud3_app_web;
+
+CREATE DATABASE ud3_balance;
+CREATE DATABASE ud3_app_web;
+
+-- ----------------- UD3: BALANCE DE INGRESOS Y GASTOS -----------------
+
+USE ud3_balance;
+
+-- Pon aquí tus CREATE TABLE, INSERT INTO y restricciones necesarias.
+
+-- ---------------------------------------------------------------------
+
+
+
+-- ------ UD3: APLICACIÓN WEB CON BBDD, SESIONES, COOKIES Y HASH -------
+
+USE ud3_app_web;
+
+-- Pon aquí tus CREATE TABLE, INSERT INTO y restricciones necesarias.
+
+-- ---------------------------------------------------------------------
