764a90a26ac37212f3edd38847cc35a62d582179
milq <miloqu@gmail.com>
2022-04-09 19:13:57 +0200

    Update course

--- cursos/dwes/ud/3/laravel/revisar/filmoteca_2/src/main/java/io/github/milq/filmoteca/PeliculaRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.milq.filmoteca;
-
-import java.util.List;
-import io.github.milq.filmoteca.Pelicula;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PeliculaRepository extends CrudRepository<Pelicula, Long> {}
