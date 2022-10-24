f1df58aefc2b5d6b9ce67992b40cf973ec3006bc
Manuel Ignacio López Quintero <miloqu@gmail.com>
2022-10-24 17:25:18 +0200

    Update

--- cursos/pria/src/Variable.cs
+++ cursos/pria/src/Variable.cs
@@ -1,14 +1,27 @@
-﻿// VARIABLES
+﻿// PRUEBA 1 - VARIABLES
 //
 // Apellidos, Nombre
 
 
 // 1. Convierte de [..]
-
+string argumento1Ejercicio1 = args[0];
+Console.WriteLine("El primer argumento del ejercicio 1 es " + argumento1Ejercicio1 + ".");
 
 
 // 2. Calcula el [...]
+string argumento1Ejercicio2 = args[0];
+Console.WriteLine("El primer argumento del ejercicio 2 es " + argumento1Ejercicio2 + ".");
 
 
 // 3. Dado cinco enteros, [...]
-int a = 
+string argumento1Ejercicio3 = args[0];
+string argumento2Ejercicio3 = args[1];
+string argumento3Ejercicio3 = args[2];
+string argumento4Ejercicio3 = args[3];
+string argumento5Ejercicio3 = args[4];
+
+int a = int.Parse(argumento1Ejercicio3);
+int b = int.Parse(argumento2Ejercicio3);
+int c = int.Parse(argumento3Ejercicio3);
+int d = int.Parse(argumento4Ejercicio3);
+int e = int.Parse(argumento5Ejercicio3);
