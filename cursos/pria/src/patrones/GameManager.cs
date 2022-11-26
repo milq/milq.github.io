4797b42816c52013362360a8b05fba714376c9c8
Manuel Ignacio López Quintero <miloqu@gmail.com>
2022-11-26 12:15:25 +0100

    Update

---
+++ cursos/pria/src/patrones/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
