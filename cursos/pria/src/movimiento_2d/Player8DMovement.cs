91c3e4e3a24931e2b1f34e90c10f52ff72326e39
Manuel Ignacio López Quintero <miloqu@gmail.com>
2022-12-08 16:38:17 +0100

    Update

---
+++ cursos/pria/src/movimiento_2d/Player8DMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Player8DMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * inputMagnitude * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
