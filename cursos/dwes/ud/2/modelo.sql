606352ba67a7f3aae1edeae972f4c012185ad557
Manuel Ignacio López Quintero <miloqu@gmail.com>
2023-10-12 17:45:51 +0200

    Create modelo.sql

---
+++ cursos/dwes/ud/2/modelo.sql
@@ -0,0 +1,11 @@
+-- Script de creación de bases de datos para MySQL/MariaDB con ejemplos
+
+DROP DATABASE IF EXISTS to2_torneos_online;
+CREATE DATABASE to2_torneos_online;
+USE to2_torneos_online;
+
+-- Pon aquí tus CREATE TABLE y restricciones necesarias
+
+
+
+-- Pon aquí tus INSERT INTO  necesarias
