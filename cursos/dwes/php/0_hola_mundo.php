4e942192b1a732f019c5d29e6cbea48268b61bb6
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-09-17 12:28:46 +0200

    Update and rename 00_hola_mundo.php to 0_hola_mundo.php

---
+++ cursos/dwes/php/0_hola_mundo.php
@@ -0,0 +1,7 @@
+<?php
+
+echo '¡Hola, mundo!';    // Imprime en la pantalla: ¡Hola, mundo!
+echo PHP_EOL;            // Imprime en la pantalla un salto de línea
+
+// 1. Comprueba que funciona PHP en una terminal con: php --version
+// 2. Ejecuta este código en una terminal con: php 0_hola_mundo.php
