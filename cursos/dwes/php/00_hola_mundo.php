faa27485c6c9f19b6c4f01069345b899bff22057
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-09-17 12:08:14 +0200

    Update 00_hola_mundo.php

--- cursos/dwes/php/00_hola_mundo.php
+++ cursos/dwes/php/00_hola_mundo.php
@@ -2,4 +2,9 @@
 
 echo 'Hello, world!' . PHP_EOL;
 
-// Ejecuta este código en una terminal con `php 00_hola_mundo.php`
+/*
+
+1. Comprueba que funciona PHP en una terminal con: php --version
+2. Ejecuta este código en una terminal con: php 00_hola_mundo.php
+
+*/
