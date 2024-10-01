77e908d1ed3a46a6a321cdbe1c0298a9fb8e292f
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-10-01 19:38:30 +0200

    Create index.php

---
+++ cursos/dwes/php/00_primera_pagina_web_php/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml' lang='es'>
+  <head>
+    <meta charset='utf-8' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    <title>Mi primera página web en PHP</title>
+  </head>
+  <body>
+    <h1>¡Hola, mundo!</h1>
+
+    <p>Esta es mi primera página web en PHP.</p>
+
+    <p><?php echo '¡Este texto está generado por PHP!'; ?></p>
+  </body>
+</html>
