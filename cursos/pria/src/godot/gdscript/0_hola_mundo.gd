92101a6e7f69dc4c69d49cecad36fc52347d7d9b
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-10-13 12:08:22 +0200

    Update 0_hola_mundo.gd

--- cursos/pria/src/godot/gdscript/0_hola_mundo.gd
+++ cursos/pria/src/godot/gdscript/0_hola_mundo.gd
@@ -3,3 +3,17 @@ extends EditorScript
 
 func _run() -> void:
     print("¡Hola, mundo!")
+
+# EXPLICACIÓN
+
+# @tool                   Indica que este 'script' es una herramienta que se
+#                         ejecutará dentro del editor de Godot.
+
+# extends EditorScript    Especifica que este 'script' hereda de EditorScript,
+#                         permitiendo interactuar con el editor.
+
+# func _run() -> void:    Define la función '_run', que es el punto de entrada
+#                         cuando el 'script' se ejecuta en el editor.
+
+# print("¡Hola, mundo!")  Imprime el mensaje "¡Hola, mundo!" en la
+#                         consola del editor.
