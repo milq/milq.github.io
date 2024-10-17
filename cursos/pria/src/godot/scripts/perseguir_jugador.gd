5d18e0e447233f77eec365b18e0f1032cc49918a
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-10-17 12:32:24 +0200

    Create perseguir_jugador.gd

---
+++ cursos/pria/src/godot/scripts/perseguir_jugador.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var jugador: Sprite2D = get_node("Jugador")
+@onready var enemigo: Sprite2D = get_node("Enemigo")
+
+const VELOCIDAD: int = 100
+
+func _process(delta: float) -> void:
+    var direccion: Vector2 = jugador.position - enemigo.position
+    var dir_normalizada: Vector2 = direccion.normalized()
+
+    enemigo.position = enemigo.position + dir_normalizada * VELOCIDAD * delta
