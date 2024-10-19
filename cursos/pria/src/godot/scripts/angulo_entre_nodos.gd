d46e7d5a502cfbe1264f38f52b16463cecbceb69
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-10-19 14:52:53 +0200

    Create angulo_entre_nodos.gd

---
+++ cursos/pria/src/godot/scripts/angulo_entre_nodos.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@onready var niblo: Sprite2D = get_node("Niblo")
+@onready var mubbit: Sprite2D = get_node("Mubbit")
+
+func _process(delta: float) -> void:
+    # Vector que apunta desde la posición de Niblo hasta la posición de Mubbit
+    var niblo_a_mubbit: Vector2 = mubbit.position - niblo.position
+
+    # Ángulo que forma el vector 'niblo_a_mubbit' con respecto a el eje x
+    var angulo_radianes: float = niblo_a_mubbit.angle()
+
+    # Convertimos el ángulo de radianes a grados y lo redondeamos
+    var angulo: float = round(rad_to_deg(angulo_radianes))
+
+    print("Posición de Niblo: ", round(niblo.position))
+    print("Posición de Mubbit: ", round(mubbit.position))
+    print("Ángulo del vector 'niblo_a_mubbit' sobre el eje x: %dº" % [angulo])
