f60600a9308dc264cc3ac825a9176b7bb24771fe
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-10-26 14:59:21 +0200

    Update navegacion.php

--- cursos/dwes/ud/3/sesiones_con_bbdd/plantillas/navegacion.php
+++ cursos/dwes/ud/3/sesiones_con_bbdd/plantillas/navegacion.php
@@ -1,14 +1,38 @@
 <?php
 $current_page = basename($_SERVER['PHP_SELF']);
-$base_url = (basename(dirname($_SERVER['PHP_SELF'])) === 'acciones') ? '../' : '';
+$subdirectory = basename(dirname($_SERVER['PHP_SELF']));
 
-$menu_items = [
-    'index.php'    => 'Inicio',
-    'ver.php'      => 'Ver',
-    'insertar.php' => 'Añadir',
-    'editar.php'   => 'Editar',
-    'borrar.php'   => 'Borrar'
-];
+if ($subdirectory === 'acciones') {
+    $menu_items = [
+        '../index.php'    => 'Inicio',
+        'ver.php'         => 'Ver',
+        'insertar.php'    => 'Añadir',
+        'editar.php'      => 'Editar',
+        'borrar.php'      => 'Borrar'
+    ];
+    $login_url = '../sesiones/iniciar_sesion.php';
+    $logout_url = '../sesiones/cerrar_sesion.php';
+} elseif ($subdirectory === 'sesiones') {
+    $menu_items = [
+        '../index.php'         => 'Inicio',
+        '../acciones/ver.php'  => 'Ver',
+        '../acciones/insertar.php' => 'Añadir',
+        '../acciones/editar.php'   => 'Editar',
+        '../acciones/borrar.php'   => 'Borrar'
+    ];
+    $login_url = 'iniciar_sesion.php';
+    $logout_url = 'cerrar_sesion.php';
+} else {
+    $menu_items = [
+        'index.php'           => 'Inicio',
+        'acciones/ver.php'    => 'Ver',
+        'acciones/insertar.php' => 'Añadir',
+        'acciones/editar.php' => 'Editar',
+        'acciones/borrar.php' => 'Borrar'
+    ];
+    $login_url = 'sesiones/iniciar_sesion.php';
+    $logout_url = 'sesiones/cerrar_sesion.php';
+}
 ?>
 <nav>
     <?php
@@ -20,18 +44,29 @@ $menu_items = [
         if ($file === $current_page) { ?>
             <?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?>
         <?php } else { ?>
-            <a href="<?= htmlspecialchars($base_url . $file, ENT_QUOTES, 'UTF-8') ?>"><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></a>
+            <a href="<?= htmlspecialchars($file, ENT_QUOTES, 'UTF-8') ?>"><?= htmlspecialchars($title, ENT_QUOTES, 'UTF-8') ?></a>
         <?php }
         $first = false;
     }
     ?>
     <span>
-        <?php if ($conectado) { ?>
-            <?= htmlspecialchars($usuario, ENT_QUOTES, 'UTF-8') ?>
-        <?php } else { ?>
-            <a href='<?= $base_url ?>iniciar_sesion.php'>Iniciar sesión</a>
-        <?php } ?>
+        <?php
+        if ($conectado) {
+            echo htmlspecialchars($usuario, ENT_QUOTES, 'UTF-8');
+        } else {
+            if ($login_url === $current_page) { ?>
+                Iniciar sesión
+            <?php } else { ?>
+                <a href="<?= htmlspecialchars($login_url, ENT_QUOTES, 'UTF-8') ?>">Iniciar sesión</a>
+            <?php }
+        }
+        ?>
          |
-        <a href='<?= $base_url ?>cerrar_sesion.php'>Cerrar sesión</a>
+        <?php
+        if ($logout_url === $current_page) { ?>
+            Cerrar sesión
+        <?php } else { ?>
+            <a href="<?= htmlspecialchars($logout_url, ENT_QUOTES, 'UTF-8') ?>">Cerrar sesión</a>
+        <?php } ?>
     </span>
 </nav>
