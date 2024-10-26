4e4826c1af54bc715d8ef6c75f958c041a873b15
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-10-26 14:51:36 +0200

    Update cabecera.php

--- cursos/dwes/ud/3/sesiones_con_bbdd/plantillas/cabecera.php
+++ cursos/dwes/ud/3/sesiones_con_bbdd/plantillas/cabecera.php
@@ -2,6 +2,8 @@
 
 $título_página = isset($título_página) ? $título_página : 'Mensajes';
 
+$subdir = basename(dirname($_SERVER['PHP_SELF']));
+$css_path = ($subdir === 'acciones' || $subdir === 'sesiones') ? '../estilo.css' : 'estilo.css';
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +11,7 @@ $título_página = isset($título_página) ? $título_página : 'Mensajes';
   <head>
     <meta charset='utf-8' />
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=2.0' />
-    <link rel='stylesheet' href='estilo.css'>
+    <link rel='stylesheet' href='<?php echo htmlspecialchars($css_path, ENT_QUOTES, 'UTF-8'); ?>'>
     <title><?php echo htmlspecialchars($título_página); ?> - Mensajes</title>
   </head>
   <body>
