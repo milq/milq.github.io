412dc7d988d9555a7a00423b60b689a9c24f78e0
Manuel Ignacio López Quintero <miloqu@gmail.com>
2024-11-27 12:10:08 +0100

    Create steering_behavior_seek.gd

---
+++ cursos/godot/scripts/steering_behavior_seek.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+# Definimos la celeridad a la que este nodo seguirá al objetivo (el ratón)
+const SPEED: float = 15.0
+
+# Distancia a partir de la cual el objeto comenzará a reducir su velocidad.
+const SLOWDOWN_DISTANCE: float = 5.0
+
+# Inicializamos la velocidad actual como un vector cero.
+var velocity: Vector2 = Vector2.ZERO
+
+func _process(delta: float) -> void:
+    # Obtenemos la posición objetivo basada en la posición del ratón.
+    var target_position: Vector2 = get_global_mouse_position()
+
+    # Calculamos la distancia entre este nodo y la posición objetivo.
+    var distance_to_target: Vector2 = target_position - self.global_position
+
+    # Calculamos la velocidad deseada normalizando la distancia y
+    # multiplicándola por la celeridad de seguimiento.
+    var desired_velocity: Vector2 = distance_to_target.normalized() * SPEED
+
+    # Calculamos el 'steering' como el ajuste necesario a la velocidad actual
+    var steering: Vector2 = desired_velocity - velocity
+
+    # Actualizamos la velocidad aplicando el steering y el tiempo transcurrido
+    velocity += steering * delta
+
+    # Calculamos el factor de reducción basado en la distancia al objetivo
+    var slow_down_factor: float = clamp(
+        distance_to_target.length() / SLOWDOWN_DISTANCE, 0.0, 1.0)
+
+    # Aplicamos el factor de reducción a la velocidad.
+    velocity *= slow_down_factor
+
+    # Actualizamos la posición del objeto basándonos en la velocidad y en delta
+    global_position += velocity
