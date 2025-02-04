d8a7fa84c30f29ae6f8a73bf7bd534a88107c956
Manuel Ignacio López Quintero <miloqu@gmail.com>
2025-02-04 19:34:46 +0100

    Update modelo.sql

--- cursos/dwes/ud/3/modelo.sql
+++ cursos/dwes/ud/3/modelo.sql
@@ -1,8 +1,8 @@
 -- Script de creación de bases de datos para MySQL/MariaDB con ejemplos
 
-DROP DATABASE IF EXISTS proyecto_3_biblioteca_virtual;
-CREATE DATABASE proyecto_3_biblioteca_virtual;
-USE proyecto_3_biblioteca_virtual;
+DROP DATABASE IF EXISTS dwes_proyecto_3_biblioteca_virtual;
+CREATE DATABASE dwes_proyecto_3_biblioteca_virtual;
+USE dwes_proyecto_3_biblioteca_virtual;
 
 -- Pon aquí debajo tus CREATE TABLE y restricciones necesarias
 
