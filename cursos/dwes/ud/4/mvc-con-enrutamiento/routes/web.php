46622e8ccb70ad9e8e76cde614f6930872f6ca7c
Manuel Ignacio López Quintero <miloqu@gmail.com>
2025-02-20 14:23:21 +0100

    Update web.php

--- cursos/dwes/ud/4/mvc-con-enrutamiento/routes/web.php
+++ cursos/dwes/ud/4/mvc-con-enrutamiento/routes/web.php
@@ -11,4 +11,7 @@ $routes->add('transactions.create', new Route(BASE_PATH . '/transactions/create'
 $routes->add('transactions.store', new Route(BASE_PATH . '/transactions/store', array('controller' => 'TransactionController', 'method'=>'store')));
 $routes->add('transactions.index', new Route(BASE_PATH . '/transactions/index', array('controller' => 'TransactionController', 'method'=>'index')));
 
+// $routes->add('transactions.destroySelect', new Route(BASE_PATH . '/transactions/destroySelect', ['controller' => 'TransactionController', 'method' => 'destroySelect']));
+// $routes->add('transactions.destroy', new Route(BASE_PATH . '/transactions/destroy', ['controller' => 'TransactionController', 'method' => 'destroy']));
+
 ?>
