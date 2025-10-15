d6caee019983387f5a53326b30bb6d33876e81f1
Manuel Ignacio López Quintero <miloqu@gmail.com>
2025-10-15 18:52:58 +0200

    Update variable increment and decrement examples

--- cursos/pria/src/python/1_variables.py
+++ cursos/pria/src/python/1_variables.py
@@ -32,12 +32,12 @@ z = x * y          # Multiplicación. Resultado: 10
 z = x / y          # División (siempre devuelve un 'float'). Resultado: 2.5
 z = x // y         # División entera. Resultado: 2
 z = x % y          # Módulo (resto de la división). Resultado: 1
-z = z + 1          # Incrementa el valor de z en 1
-z = z - 1          # Disminuye el valor de z en 1
+z = z + 1          # Incrementa el valor de z en 1. Resultado: 2
+z = z - 1          # Disminuye el valor de z en 1. Resultado: 1
 
 # Puedes usar operadores abreviados (z += 1, z -= 1, z *= 2, z /= 3, etc):
-z += 5             # Equivale a z = z + 5
-z -= 2             # Equivale a z = z - 2
+z += 5             # Equivale a z = z + 5. Resultado: 6
+z -= 2             # Equivale a z = z - 2. Resultado: 4
 
 
 # OPERACIONES BÁSICAS CON CADENAS DE TEXTO (strings)
