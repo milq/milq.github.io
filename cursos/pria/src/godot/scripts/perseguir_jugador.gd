591014aec02ad75946d91f977e6faa45125d7d4c
Manuel Ignacio López Quintero <miloqu@gmail.com>
2025-10-29 17:55:57 +0100

    Update perseguir_jugador.gd

--- cursos/pria/src/godot/scripts/perseguir_jugador.gd
+++ cursos/pria/src/godot/scripts/perseguir_jugador.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-@onready var jugador: Sprite2D = get_node("Jugador")
-@onready var enemigo: Sprite2D = get_node("Enemigo")
+@onready var niblo: Sprite2D = get_node("Niblo")
+@onready var mubbit: Sprite2D = get_node("Mubbit")
 
 const VELOCIDAD: int = 100
 
 func _process(delta: float) -> void:
-    # Vector desde la posición del enemigo hasta la del jugador
-    var vector_hacia_jugador: Vector2 = jugador.position - enemigo.position
+    # Vector desde la posición de Mubbit hasta la de Niblo
+    var vector_hacia_niblo: Vector2 = niblo.position - mubbit.position
 
-    # Normaliza el vector 'vector_hacia_jugador' para que su magnitud sea uno
-    var normalizado: Vector2 = vector_hacia_jugador.normalized()
+    # Normaliza el vector 'vector_hacia_niblo' para que su magnitud sea uno
+    var normalizado: Vector2 = vector_hacia_niblo.normalized()
 
-    # Actualiza la posición del enemigo hacia el jugador a la velocidad dada
-    enemigo.position = enemigo.position + normalizado * VELOCIDAD * delta
+    # Actualiza la posición de Mubbit hacia Niblo a la velocidad dada
+    mubbit.position = mubbit.position + normalizado * VELOCIDAD * delta
